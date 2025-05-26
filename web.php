8c2f6faeaa4f82bffa96da74eb04b3a69c24f820
Gabrielseilhe <168229694+gaberilseilhe@users.noreply.github.com>
2025-05-26 11:41:50 -0300

    Add files via upload

---
+++ web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ServicosController;
+use App\Http\Controllers\AgendamentoController;
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\GruposController;
+
+
+Route::get('usuarios', [UserController::class, 'getusuarios']);
+
+Route::get('servicos', [ServicosController::class, 'getservicos']);
+
+Route::get('grupos', [GruposController::class, 'getgrupos']);
+
+Route::get('agendamento', [AgendamentoController::class, 'getagendamento']);
+
+Route::get('agenda', [AgendaController::class, 'getagenda']);
+
+
+
+
+Route::get('/criarUsuario', [UserController::class, 'criarUsuario']);
+Route::get('/criarServico', [ServicosController::class, 'criarServico']);
+Route::get('/criarGrupo', [GruposController::class, 'criarGrupo']);
+Route::get('/criarAgendamento', [AgendamentoController::class, 'criarAgendamento']);
+Route::get('/criarAgenda', [AgendaController::class, 'criarAgenda']);
+
+
+
+Route::get('deletarUsuario/{id_usuario}', [UserController::class, 'deletarUsuario']);
+Route::get('deletarServico/{id_servico}', [ServicosController::class, 'deletarServico']);
+Route::get('deletarGrupo/{id_grupo}', [GruposController::class, 'deletarGrupo']);
+Route::get('deletarAgendamento/{id_agendamento}', [AgendamentoController::class, 'deletarAgendamento']);
+Route::get('deletarAgenda/{id_agenda}', [AgendaController::class, 'deletarAgenda']);
+
+
+Route::get('atualizarUsuario/{id_usuario}', [UserController::class, 'atualizarUsuario']);
+Route::get('atualizarServico/{id_servico}', [ServicosController::class, 'atualizarServico']);
+Route::get('atualizarGrupo/{id_grupo}', [GruposController::class, 'atualizarGrupo']);
+Route::get('atualizarAgendamento/{id_agendamento}', [AgendamentoController::class, 'atualizarAgendamento']);
+Route::get('atualizarAgenda/{id_agenda}', [AgendaController::class, 'atualizarAgenda']);
+
+
+Route::get('acharUsuario/{id_usuario}', [UserController::class, 'acharUsuario']);
+Route::get('acharServico/{id_servico}', [ServicosController::class, 'acharServico']);
+Route::get('acharGrupo/{id_grupo}', [GruposController::class, 'acharGrupo']);
+Route::get('acharAgendamento/{id_agendamento}', [AgendamentoController::class, 'acharAgendamento']);
+Route::get('acharAgenda/{id_agenda}', [AgendaController::class, 'acharAgenda']);
